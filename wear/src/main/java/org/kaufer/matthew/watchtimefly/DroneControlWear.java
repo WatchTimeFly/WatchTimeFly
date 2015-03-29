fd87b03c3ba05334a1ce470e9c3e7957fe5a8db7
Matthew Kaufer <mjkaufer@gmail.com>
2015-03-29 06:03:33 +0100

    Fixed buttons

--- wear/src/main/java/org/kaufer/matthew/watchtimefly/DroneControlWear.java
+++ wear/src/main/java/org/kaufer/matthew/watchtimefly/DroneControlWear.java
@@ -1,32 +1,32 @@
 package org.kaufer.matthew.watchtimefly;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.view.GestureDetector.OnDoubleTapListener;
 
 public class DroneControlWear extends Activity {
-
-    private TextView mTextView;
     private Button yawLeft, yawRight, altitudeAscend, altitudeDescend, pitchForwards, pitchBackwards, kill, takeoff, land;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drone_control_wear);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-            @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
-            }
-        });
 
+//        Button b = (Button)findViewById(R.id.button);
+//
+//        b.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.d("test","clicked the button");
+//                v.setBackgroundColor(Color.argb(255, 0, 255, 255));
+//            }
+//        });
 
         yawLeft = (Button)findViewById(R.id.yawLeft);
         yawRight = (Button)findViewById(R.id.yawRight);
@@ -38,233 +38,31 @@ public class DroneControlWear extends Activity {
         takeoff = (Button)findViewById(R.id.takeoff);
         land = (Button)findViewById(R.id.land);
 
-
-//        addAllListeners();
-
-    }
-
-    public void addListenerOld(View v, GestureDetector.SimpleOnGestureListener listener){
-        final GestureDetector gd = new GestureDetector(getApplicationContext(), listener);
-
-        v.setOnTouchListener(new View.OnTouchListener(){
+        addListener(yawLeft, new View.OnClickListener() {
             @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                return gd.onTouchEvent(motionEvent);
-            }
-        });
-    }
-
-    public void stopDrone(){
-        //stops drone;
-    }
-    //click is when you release the button, touch is when you first touch it
-    public void addListener(Button b, View.OnClickListener ocl, View.OnTouchListener otl){
-        b.setOnClickListener(ocl);
-        b.setOnTouchListener(otl);
-    }
-    //for drone control -
-
-    public void addAllListeners(){
-
-        addListener(yawLeft, new View.OnClickListener(){
-            public void onClick(View v){
-                stopDrone();
-            }
-        }, new View.OnTouchListener(){
-            public boolean onTouch(View v, MotionEvent e){
-                //do some movement on the drone
-                return true;
+            public void onClick(View v) {
+                Log.d("move","left turn");
             }
         });
 
-        addListener(yawRight, new View.OnClickListener(){
-            public void onClick(View v){
-                stopDrone();
-            }
-        }, new View.OnTouchListener(){
-            public boolean onTouch(View v, MotionEvent e){
-                //do some movement on the drone
-                return true;
-            }
-        });
-
-        addListener(altitudeAscend, new View.OnClickListener(){
-            public void onClick(View v){
-                stopDrone();
-            }
-        }, new View.OnTouchListener(){
-            public boolean onTouch(View v, MotionEvent e){
-                //do some movement on the drone
-                return true;
-            }
-        });
-
-        addListener(altitudeDescend, new View.OnClickListener(){
-            public void onClick(View v){
-                stopDrone();
-            }
-        }, new View.OnTouchListener(){
-            public boolean onTouch(View v, MotionEvent e){
-                //do some movement on the drone
-                return true;
-            }
-        });
-
-        addListener(pitchBackwards, new View.OnClickListener(){
-            public void onClick(View v){
-                stopDrone();
-            }
-        }, new View.OnTouchListener(){
-            public boolean onTouch(View v, MotionEvent e){
-                //do some movement on the drone
-                return true;
-            }
-        });
-
-        addListener(pitchForwards, new View.OnClickListener(){
-            public void onClick(View v){
-                stopDrone();
-            }
-        }, new View.OnTouchListener(){
-            public boolean onTouch(View v, MotionEvent e){
-                //do some movement on the drone
-                return true;
-            }
-        });
-
-        addListener(kill, new View.OnClickListener(){
-            public void onClick(View v){
-                stopDrone();
-            }
-        }, new View.OnTouchListener(){
-            public boolean onTouch(View v, MotionEvent e){
-                //do some movement on the drone
-                return true;
-            }
-        });
-
-        addListener(land, new View.OnClickListener(){
-            public void onClick(View v){
-                stopDrone();
-            }
-        }, new View.OnTouchListener(){
-            public boolean onTouch(View v, MotionEvent e){
-                //do some movement on the drone
-                return true;
-            }
-        });
-
-        addListener(takeoff, new View.OnClickListener(){
-            public void onClick(View v){
-                stopDrone();
-            }
-        }, new View.OnTouchListener(){
-            public boolean onTouch(View v, MotionEvent e){
-                //do some movement on the drone
-                return true;
-            }
-        });
-
-    }
-
-    public void addAllListenersOld(){
-        //adds all of the listeners
-        addListenerOld(yawLeft, new GestureDetector.SimpleOnGestureListener(){
+        addListener(yawRight, new View.OnClickListener() {
             @Override
-            public boolean onDoubleTap(MotionEvent e){
-                //special double click thing
-                return true;
-            }
-
-            @Override
-            public boolean onSingleTapConfirmed(MotionEvent e){
-                //science!
-                return true;
+            public void onClick(View v) {
+                Log.d("move","right turn");
             }
         });
 
-        addListenerOld(yawRight, new GestureDetector.SimpleOnGestureListener(){
+        addListener(altitudeDescend, new View.OnClickListener() {
             @Override
-            public boolean onDoubleTap(MotionEvent e){
-                //special double click thing
-                return true;
-            }
+            public void onClick(View v) {
 
-            @Override
-            public boolean onSingleTapConfirmed(MotionEvent e){
-                //science!
-                return true;
-            }
-        });
-
-        addListenerOld(altitudeAscend, new GestureDetector.SimpleOnGestureListener(){
-            @Override
-            public boolean onDoubleTap(MotionEvent e){
-                //special double click thing
-                return true;
-            }
-
-            @Override
-            public boolean onSingleTapConfirmed(MotionEvent e){
-                //science!
-                return true;
-            }
-        });
-
-        addListenerOld(altitudeDescend, new GestureDetector.SimpleOnGestureListener(){
-            @Override
-            public boolean onDoubleTap(MotionEvent e){
-                //special double click thing
-                return true;
-            }
-
-            @Override
-            public boolean onSingleTapConfirmed(MotionEvent e){
-                //science!
-                return true;
-            }
-        });
-
-        addListenerOld(pitchBackwards, new GestureDetector.SimpleOnGestureListener(){
-            @Override
-            public boolean onDoubleTap(MotionEvent e){
-                //special double click thing
-                return true;
-            }
-
-            @Override
-            public boolean onSingleTapConfirmed(MotionEvent e){
-                //science!
-                return true;
-            }
-        });
-
-        addListenerOld(pitchForwards, new GestureDetector.SimpleOnGestureListener(){
-            @Override
-            public boolean onDoubleTap(MotionEvent e){
-                //special double click thing
-                return true;
-            }
-
-            @Override
-            public boolean onSingleTapConfirmed(MotionEvent e){
-                //science!
-                return true;
-            }
-        });
-
-        addListenerOld(kill, new GestureDetector.SimpleOnGestureListener(){
-            @Override
-            public boolean onDoubleTap(MotionEvent e){
-                //special double click thing
-                return true;
-            }
-
-            @Override
-            public boolean onSingleTapConfirmed(MotionEvent e){
-                //science!
-                return true;
             }
         });
     }
+
+    public void addListener(Button b, View.OnClickListener l){
+        b.setOnClickListener(l);
+    }
+
+
 }
