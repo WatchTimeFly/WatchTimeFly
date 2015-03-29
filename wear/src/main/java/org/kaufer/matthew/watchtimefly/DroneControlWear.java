323e8787ea515ebb7ba4bcdc4cc2306e3f86df61
Matthew Kaufer <mjkaufer@gmail.com>
2015-03-29 04:00:28 +0100

    Rearranged buttons, added to java

--- wear/src/main/java/org/kaufer/matthew/watchtimefly/DroneControlWear.java
+++ wear/src/main/java/org/kaufer/matthew/watchtimefly/DroneControlWear.java
@@ -3,6 +3,7 @@ package org.kaufer.matthew.watchtimefly;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class DroneControlWear extends Activity {
@@ -20,5 +21,16 @@ public class DroneControlWear extends Activity {
                 mTextView = (TextView) stub.findViewById(R.id.text);
             }
         });
+
+        Button yawLeft, yawRight, altitudeAscend, altitudeDescend, pitchForwards, pitchBackwards, kill;
+
+        yawLeft = (Button)findViewById(R.id.yawLeft);
+        yawRight = (Button)findViewById(R.id.yawRight);
+        altitudeAscend = (Button)findViewById(R.id.altitudeAscend);
+        altitudeDescend = (Button)findViewById(R.id.altitudeDescend);
+        pitchForwards = (Button)findViewById(R.id.pitchForwards);
+        pitchBackwards = (Button)findViewById(R.id.pitchBackwards);
+        kill = (Button)findViewById(R.id.kill);
+
     }
 }
